861472b6b95b71d578e3e4ac444b81c5bd50b818
Ayesha <aliayeshanyc@gmail.com>
2018-12-11 09:45:54 -0500

    stats_table

--- data_server/data_server.js
+++ data_server/data_server.js
@@ -23,6 +23,7 @@ app.get('/', function(request, response){
 app.get('/login', function(request, response){
   var user_data={
       name: request.query.player_name
+      // pswd: request.query.psw
   };
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
@@ -48,12 +49,25 @@ app.get('/rules', function(request, response){
 app.get('/stats', function(request, response){
   //load csv
   var users_file = fs.readFileSync("data/users.csv", "utf8");
-  console.log(users_file);
   //parse csv
+  var rows = users_file.split('\r\n');
+  console.log(rows);
+  var user_data = [];
+  for(var i = 1; i < rows.length; i++) {
+      var user_d = rows[i].trim().split(",");
+      var user = {};
+      user["name"] = user_d[0];
+      user["total_games"] = user_d[1];
+      user["wins"] = user_d[2];
+      user["losses"] = user_d[3];
+      user_data.push(user);
+  }
+  console.log(user_data);
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.render('stats');
+  response.render('stats', {user:user_data});
 });
+
 app.get('/about', function(request, response){
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
