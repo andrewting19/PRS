b8e40e2ab21fc376451b8845908f9c54a2c70220
ayeshaali <aliayeshanyc@gmail.com>
2018-12-11 19:38:28 -0500

    login

--- data_server/data_server.js
+++ data_server/data_server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var fs = require('fs');
 var favicon = require('serve-favicon');
-
-
 var app = express();
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
@@ -21,13 +19,22 @@ app.get('/', function(request, response){
 });
 
 app.get('/login', function(request, response){
-  var user_data={
-      name: request.query.player_name
-      // pswd: request.query.psw
-  };
+  var user_data={};
+  user_data["name"] = request.query.player_name;
+  var csv_data = loadCSV();
+  for (var i = 0; i < csv_data.length; i++) {
+    if (csv_data[i].name == user_data["name"]) {
+      if (csv_data[i].pswd == request.query.pswd) {
+        response.status(200);
+        response.setHeader('Content-Type', 'text/html')
+        response.render('game', {user:user_data});
+        break;
+      }
+    }
+  }
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.render('game', {user:user_data});
+  response.render('index');
 });
 
 app.get('/:user/results', function(request, response){
@@ -47,22 +54,7 @@ app.get('/rules', function(request, response){
 });
 
 app.get('/stats', function(request, response){
-  //load csv
-  var users_file = fs.readFileSync("data/users.csv", "utf8");
-  //parse csv
-  var rows = users_file.split('\r\n');
-  console.log(rows);
-  var user_data = [];
-  for(var i = 1; i < rows.length; i++) {
-      var user_d = rows[i].trim().split(",");
-      var user = {};
-      user["name"] = user_d[0];
-      user["total_games"] = user_d[1];
-      user["wins"] = user_d[2];
-      user["losses"] = user_d[3];
-      user_data.push(user);
-  }
-  console.log(user_data);
+  var user_data = loadCSV();
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
   response.render('stats', {user:user_data});
@@ -73,3 +65,25 @@ app.get('/about', function(request, response){
   response.setHeader('Content-Type', 'text/html')
   response.render('about');
 });
+
+function loadCSV() {
+  var users_file = fs.readFileSync("data/users.csv", "utf8");
+  //parse csv
+  var rows = users_file.split('\r\n');
+  console.log(rows);
+  var user_data = [];
+  for(var i = 1; i < rows.length; i++) {
+      var user_d = rows[i].trim().split(",");
+      var user = {};
+      user["name"] = user_d[0];
+      user["pswd"] = user_d[1];
+      user["total_games"] = user_d[2];
+      user["wins"] = user_d[3];
+      user["losses"] = user_d[4];
+      user["rock"] = user_d[5];
+      user["paper"] = user_d[6];
+      user["scissors"] = user_d[7];
+      user_data.push(user);
+  }
+  return user_data;
+}
