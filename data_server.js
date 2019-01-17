430f784518df85507cf4bf2c27c3b420d3c37c42
sadigulcelik <sadigulcelik@gmail.com>
2019-01-16 22:39:41 -0500

    Revert "onegood"
    This reverts commit 90a787e49bdad532ad1e195a9b94e09f5d76a318.

--- data_server.js
+++ data_server.js
@@ -24,7 +24,11 @@ for (var k=0;k<svgNames.length;k++){
     var output=out[0];
     for(var i=1;i<out.length;i++){
 <<<<<<< HEAD
+<<<<<<< HEAD
         output+="fill:"+chosenColor+out[i].substring(out[i].indexOf('"'),out[i].length);
+=======
+        output+="fill:"+colors[Math.floor(Math.random()*colors.length)]+out[i].substring(out[i].indexOf("\""),out.length);
+>>>>>>> parent of 90a787e... onegood
 =======
         output+="fill:"+colors[Math.floor(Math.random()*colors.length)]+out[i].substring(out[i].indexOf("\""),out.length);
 >>>>>>> parent of 90a787e... onegood
