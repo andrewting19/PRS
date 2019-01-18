a41d48a32bfb9dbee19c48fcc43ec38f9349647a
sadigulcelik <sadigulcelik@gmail.com>
2019-01-17 20:45:33 -0500

    fixed

--- data_server.js
+++ data_server.js
@@ -19,9 +19,10 @@ var svgExtensions=["_waiting","_rock","_scissors","_paper"];
 
 //changes the colors of the svgs (currently called every login)
 function changeColors(){
+    tempColors=colors.splice();
   for (var k=0;k<svgNames.length;k++){
-    var index =Math.floor(Math.random()*colors.length)
-    chosenColor=colors[index]
+    var index =Math.floor(Math.random()*tempColors.length)
+    chosenColor=tempColors[index]
     for (var j=0; j<svgExtensions.length;j++){
       svgName=__dirname+"/public/images/"+svgNames[k]+svgExtensions[j]+".svg";
       if(!svgName.includes("regal_waiting")&&!svgName.includes("the_boss_waiting")){
@@ -34,10 +35,9 @@ function changeColors(){
         fs.writeFileSync(svgName,output, "utf8");
       }
     }
-    colors.splice(index,1)
+    tempColors.splice(index,1)
   }
 }
-
 //set up server
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
