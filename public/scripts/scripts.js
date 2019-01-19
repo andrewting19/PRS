cad3c9c57177363f8b65d6bc494e1124b87a90d7
sadigulcelik <sadigulcelik@gmail.com>
2019-01-18 22:06:16 -0500

    now

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,11 +1,14 @@
 
 if(document.title==="P,R,S - Game"){
+    document.getElementById("hurry?").innerHTML="Play the Game";
     document.getElementById("martin atkinson").src='/images/ref/three.svg';
     setTimeout(function(){document.getElementById("martin atkinson").src='/images/ref/two.svg';}, 1000);
         
     setTimeout(function(){document.getElementById("martin atkinson").src='/images/ref/one.svg';}, 2000);
         
     setTimeout(function(){document.getElementById("martin atkinson").src='/images/ref/fairy_dust.svg';}, 3000);
+    
+    setTimeout(function(){document.getElementById("martin atkinson").src='/images/ref/watch.svg'; document.getElementById("hurry?").innerHTML="Play the Game; Hurry!";}, 8000);
     
 }
 if(document.title==="P,R,S - Results"){
